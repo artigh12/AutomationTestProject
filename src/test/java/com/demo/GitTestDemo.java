e95fcfe2edfc3191612c23ec0205b297c8a6be29
artigh12 <artigharjale96@gmail.com>
2024-06-26 12:50:26 +0530

    automation test project added

---
+++ src/test/java/com/demo/GitTestDemo.java
@@ -0,0 +1,23 @@
+package com.demo;
+public class GitTestDemo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("git config --global user.name 'USERNAME'");
+		
+		System.out.println("git config --global user.email 'emailId'");
+		
+		System.out.println("git init");
+		
+		System.out.println("git remote add origin projectUrl");
+		
+		System.out.println("git add .");
+		
+		System.out.println("git commit -m 'message'");
+		
+		System.out.println("git push origin master");
+
+
+	}
+
+}
